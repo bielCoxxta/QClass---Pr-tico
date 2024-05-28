0b16e4a974cce8b008def04eea2327926077c08e
Gabriel <gabriel.costa@quadritech.com.br>
2024-05-28 04:12:25 -0300

    feat: update native files

--- core/lib/core.dart
+++ core/lib/core.dart
@@ -1,11 +1,16 @@
 library;
 
+export 'package:cloud_firestore/cloud_firestore.dart';
+export 'package:core/src/theme_system/presentation/controllers/theme_controller.dart';
 export 'package:core/src/utils/crash_log.dart';
+export 'package:core/src/utils/navigation/domain/navigator_manager.dart';
 export 'package:core/src/utils/router_observer.dart';
 export 'package:core/src/utils/state_management/domain/interfaces/interfaces/custom_controller.dart';
+export 'package:firebase_core/firebase_core.dart';
+export 'package:firebase_in_app_messaging/firebase_in_app_messaging.dart';
+export 'package:flutter_localizations/flutter_localizations.dart';
 export 'package:flutter_modular/flutter_modular.dart';
 export 'package:flutter_screenutil/flutter_screenutil.dart';
-export 'package:flutter_localizations/flutter_localizations.dart';
 
 export 'src/base_path.dart';
 export 'src/core_base.dart';
