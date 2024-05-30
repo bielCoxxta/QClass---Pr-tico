072dbfd89290e8ef3a8719bb1dd5e05936f5d875
Gabriel <costa@espressolabs.com.br>
2024-05-29 23:25:28 -0300

    feat: update login page

--- auth/lib/src/presentation/pages/login/login_page.dart
+++ auth/lib/src/presentation/pages/login/login_page.dart
@@ -12,6 +12,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final infoApp = Modular.get<PackageInfoEntity>();
+
   final cpfField = const ValueKey('cpf_field');
   final cpfController = TextEditingController();
   final passController = TextEditingController();
@@ -41,7 +43,7 @@ class _LoginPageState extends State<LoginPage> {
 
   void checkButton() {
     setState(() {
-      activeButton == (cpfIsValid & passIsValid);
+      activeButton = (cpfIsValid & passIsValid);
     });
   }
 
@@ -51,131 +53,157 @@ class _LoginPageState extends State<LoginPage> {
       backgroundColor: context.isDarkMode
           ? _colorScheme.secondary
           : _colorScheme.onBackground,
-      body: Align(
-        alignment: Alignment.bottomCenter,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: Spacing.xxxl.value,
+      body: FullSizeScrollableWidget(
+        child: SafeArea(
+          bottom: false,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                  icon: Icon(
+                    x.state == ThemeMode.dark
+                        ? Icons.brightness_4_rounded
+                        : Icons.brightness_6_rounded,
+                    color: context.colorScheme.background,
+                  ),
+                  style: IconButton.styleFrom(
+                    backgroundColor: context.colorScheme.onBackground,
+                  ),
+                  onPressed: () {
+                    x.changeTheme();
+                  },
+                ),
               ),
-              child: CustomImage(
-                svgAsset: AssetsPath.qclassLogo2,
-                packageName: AssetsPath.packageName,
-                imageColor: AppColorsBase.white,
+              const Spacer(),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: Spacing.xxxl.value,
+                ),
+                child: CustomImage(
+                  svgAsset: AssetsPath.qclassLogo2,
+                  packageName: AssetsPath.packageName,
+                  imageColor: AppColorsBase.white,
+                ),
               ),
-            ),
-            TextButton(
-              onPressed: () {
-                x.changeTheme();
-              },
-              style: TextButton.styleFrom(backgroundColor: Colors.white),
-              child: Text('Tema: ${x.state.name}'),
-            ),
-            // === form ===
-
-            ClipRRect(
-              borderRadius: BorderRadius.only(
-                topLeft: AppThemeBase.radiusCircular,
-                topRight: AppThemeBase.radiusCircular,
-              ),
-              child: ColoredBox(
-                color: _colorScheme.background,
-                child: SizedBox(
-                  width: double.infinity,
-                  child: SafeArea(
-                    top: false,
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: Spacing.md.value,
-                      ),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Spacing.md.vertical,
-                          Center(
-                            child: Text(
-                              'Entre com seu usuário',
-                              style: _textTheme.titleMedium,
-                            ),
+              const Spacer(),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: AppThemeBase.radiusCircular,
+                    topRight: AppThemeBase.radiusCircular,
+                  ),
+                  child: ColoredBox(
+                    color: _colorScheme.background,
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: SafeArea(
+                        top: false,
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: Spacing.md.value,
                           ),
-                          Spacing.md.vertical,
-                          CustomInputField(
-                            key: cpfField,
-                            enabled: !isLoading,
-                            controller: cpfController,
-                            hintText: '000.000.000-00',
-                            keyboardType: TextInputType.number,
-                            validators: const [
-                              FormValidators.emptyField,
-                              FormValidators.invalidCPF,
-                            ],
-                            inputFormatters: [
-                              FilteringTextInputFormatter.digitsOnly,
-                              FormMasks.cpf(),
-                            ],
-                            onChanged: validateCpf,
-                            labelWidget: const InputLabel(
-                              label: 'Informe seu CPF',
-                            ),
-                          ),
-                          Spacing.sm.vertical,
-                          CustomInputField(
-                            hintText: 'Digite aqui',
-                            controller: passController,
-                            enabled: !isLoading,
-                            obscureText: hidePassword,
-                            labelWidget: const InputLabel(label: 'Senha'),
-                            onChanged: validatePass,
-                            suffixIcon: IconButton(
-                              icon: Icon(
-                                hidePassword
-                                    ? Icons.visibility
-                                    : Icons.visibility_off_rounded,
-                                color: _colorScheme.onBackground,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Spacing.md.vertical,
+                              Center(
+                                child: Text(
+                                  'Entre com seu usuário',
+                                  style: _textTheme.titleMedium,
+                                ),
                               ),
-                              onPressed: () async {
-                                setState(() {
-                                  hidePassword = !hidePassword;
-                                });
-                              },
-                            ),
-                          ),
-                          Spacing.sm.vertical,
-                          SizedBox(
-                            width: double.infinity,
-                            child: Text(
-                              'Esqueci a senha',
-                              style: _textTheme.bodyLarge,
-                              textAlign: TextAlign.right,
-                            ),
-                          ),
-                          Spacing.xxxl.vertical,
-                          CustomButton.text(
-                            isEnabled: activeButton,
-                            isLoading: isLoading,
-                            text: 'Entrar',
-                            onPressed: () {},
-                          ),
-                          Spacing.md.vertical,
-                          Center(
-                            child: Text(
-                              'Version 2.2.3',
-                              style: context.textTheme.labelMedium?.copyWith(
-                                color: AppColorsBase.info,
+                              Spacing.md.vertical,
+                              CustomInputField(
+                                key: cpfField,
+                                enabled: !isLoading,
+                                controller: cpfController,
+                                hintText: '000.000.000-00',
+                                keyboardType: TextInputType.number,
+                                validators: const [
+                                  FormValidators.emptyField,
+                                  FormValidators.invalidCPF,
+                                ],
+                                inputFormatters: [
+                                  FilteringTextInputFormatter.digitsOnly,
+                                  FormMasks.cpf(),
+                                ],
+                                onChanged: validateCpf,
+                                labelWidget: const InputLabel(
+                                  label: 'Informe seu CPF',
+                                ),
                               ),
-                            ),
+                              Spacing.sm.vertical,
+                              CustomInputField(
+                                hintText: 'Digite aqui',
+                                controller: passController,
+                                enabled: !isLoading,
+                                obscureText: hidePassword,
+                                labelWidget: const InputLabel(label: 'Senha'),
+                                onChanged: validatePass,
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    hidePassword
+                                        ? Icons.visibility
+                                        : Icons.visibility_off_rounded,
+                                    color: _colorScheme.onBackground,
+                                  ),
+                                  onPressed: () async {
+                                    setState(() {
+                                      hidePassword = !hidePassword;
+                                    });
+                                  },
+                                ),
+                              ),
+                              Spacing.xs.vertical,
+                              InkWell(
+                                onTap: () {},
+                                child: Padding(
+                                  padding: EdgeInsets.symmetric(
+                                    vertical: Spacing.xxs.value,
+                                    horizontal: 2.responsiveWidth,
+                                  ),
+                                  child: Align(
+                                    alignment: Alignment.centerRight,
+                                    child: Text(
+                                      'Esqueci a senha',
+                                      style: _textTheme.labelMedium,
+                                      textAlign: TextAlign.right,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Spacing.xxxl.vertical,
+                              CustomButton.text(
+                                isEnabled: activeButton,
+                                isLoading: isLoading,
+                                text: 'Entrar',
+                                onPressed: () {},
+                              ),
+                              Spacing.xxxl.vertical,
+                              Center(
+                                child: Text(
+                                  'Version ${infoApp.version}',
+                                  style:
+                                      context.textTheme.labelMedium?.copyWith(
+                                    color: AppColorsBase.info,
+                                  ),
+                                ),
+                              ),
+                              Spacing.md.vertical,
+                            ],
                           ),
-                        ],
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
