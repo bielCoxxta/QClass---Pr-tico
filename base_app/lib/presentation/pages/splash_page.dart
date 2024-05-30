c6c9dfa0954c5e4a3160797a3d462ed5e1ac9c91
Gabriel <costa@espressolabs.com.br>
2024-05-29 23:24:16 -0300

    refactor: update splash page

--- base_app/lib/presentation/pages/splash_page.dart
+++ base_app/lib/presentation/pages/splash_page.dart
@@ -2,6 +2,8 @@ import 'package:auth/auth.dart';
 import 'package:core/core.dart';
 import 'package:flutter/material.dart';
 
+import '../controllers/init_app_controller.dart';
+
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
 
@@ -10,35 +12,31 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
-  final x = Modular.get<ThemeController>();
+  final initAppController = Modular.get<InitAppController>();
 
   @override
   void initState() {
     super.initState();
+    loadingApp();
     // verifica seção do usuário
     Modular.to.navigate(AuthRoutes.root.path);
   }
 
+  void loadingApp() async {
+    await initAppController.fetchInfoApp().then((value) {
+      // como essa instância deve ser chamada antes do RunApp foi necessário criar uma instância
+      // de mock e substituir a mesma quando tiver carregado os dados do package
+      if (value != null) Modular.replaceInstance(value);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children: [
-            ValueListenableBuilder<ThemeMode>(
-              valueListenable: x,
-              builder: (context, state, _) {
-                return Text(state.name);
-              },
-            ),
-            TextButton(
-              onPressed: () {
-                x.changeTheme();
-              },
-              child: const Text('Trocar thema'),
-            )
-          ],
+          children: [],
         ),
       ),
     );
